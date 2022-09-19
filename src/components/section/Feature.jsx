c5669c040d120973e60004d87f310ab7600a8121
Muhibbullah Ansary <dev.muhibbullah@gmail.com>
2022-09-19 22:44:38 +0600

    feature section component created

---
+++ src/components/section/Feature.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Feature() {
+  return (
+    <main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <div className="px-4 py-6 sm:px-0">
+            <div className="h-96 rounded-lg border-4 border-dashed border-gray-200"></div>
+        </div>
+        </div>
+    </main>
+  )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import Feature from '../components/section/Feature'
 
 export default function Home() {
   return (
     <>
       <Banner />
-      <main>
-        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-          <div className="px-4 py-6 sm:px-0">
-            <div className="h-96 rounded-lg border-4 border-dashed border-gray-200"></div>
-          </div>
-        </div>
-      </main>
+      <Feature />
     </>
   )
 }
