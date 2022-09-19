480e0f1455e35ac24acb393e73d6cb07bf1f27bb
Muhibbullah Ansary <dev.muhibbullah@gmail.com>
2022-09-19 22:30:52 +0600

    first commit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,7 +5,7 @@ export default function Navigation()
 {
     const [mobileOpen, setMobileOpen] = useState(false)
     return (
-        <nav className="bg-gray-800">
+        <nav className="bg-white">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="flex h-16 items-center justify-between">
                     <div className="flex items-center">
@@ -14,11 +14,11 @@ export default function Navigation()
                         </div>
                         <div className="hidden md:block">
                             <div className="ml-10 flex items-baseline space-x-4">
-                                <a href="#demo" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Dashboard</a>
-                                <a href="#demo" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
-                                <a href="#demo" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
-                                <a href="#demo" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
-                                <a href="#demo" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Reports</a>
+                                <a href="#demo" className="text-black px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
+                                <a href="#demo" className="text-gray-400 hover:text-black px-3 py-2 rounded-md text-sm font-medium">Team</a>
+                                <a href="#demo" className="text-gray-400 hover:text-black px-3 py-2 rounded-md text-sm font-medium">Projects</a>
+                                <a href="#demo" className="text-gray-400 hover:text-black px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
+                                <a href="#demo" className="text-gray-400 hover:text-black px-3 py-2 rounded-md text-sm font-medium">Reports</a>
                             </div>
                         </div>
                     </div>
